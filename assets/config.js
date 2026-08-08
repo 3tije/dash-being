ceba6f95d221e4ffeb01a5840fcb771717c924c6
3tije <b177tri@gmail.com>
2026-08-09 05:27:41 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -4,7 +4,7 @@
 */
 window.BEING_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyD82puMnBJXTQcCkSKGBJ1j15LOxKPJPiNOEe6jmE6F8YgJWmkH1lrKRwcC8ZFjKR-/exec",
-  CONSULTATION_URL: "https://3tije.github.io/being/",
+  CONSULTATION_URL: "konsultasi.html",
   LMS_URL: "https://3tije.github.io/lms-being/",
   VOICE_URL: "suara-anda.html",
   ADMIN_URL: "admin.html"
