c28cd4eb83738ef47d1c9e87536ac5b33ed4c1c2
3tije <b177tri@gmail.com>
2026-08-10 12:50:18 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -5,7 +5,7 @@
 window.BEING_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyD82puMnBJXTQcCkSKGBJ1j15LOxKPJPiNOEe6jmE6F8YgJWmkH1lrKRwcC8ZFjKR-/exec",
   CONSULTATION_URL: "konsultasi.html",
-  LMS_URL: "https://beingpsikologi.github.io/bangdir/",
+  LMS_URL: "https://bangdir.beingpsikologi.com/",
   VOICE_URL: "suara-anda.html",
   ADMIN_URL: "admin.html"
 };
